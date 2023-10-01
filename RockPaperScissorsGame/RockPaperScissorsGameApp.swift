de7bbe248539b2959971296986faa7f33deda224
minhye9731 <jenney62639731@gmail.com>
2023-10-01 17:15:53 +0900

    Initial Commit

---
+++ RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by 강민혜 on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
